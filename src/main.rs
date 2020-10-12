b3fbcdf7ee481174c5ea743d352b4854a7ece158
Christopher Bateman <batemanc@lincolnagritech.co.nz>
2020-10-12 21:44:05 +1300

    Method for exporting images added to test. Bug fixes in Shepp-Logan method.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
+
+
 use ndarray::Array2;
+use image::GrayImage;
 
 fn main() {
-    let img = shepp_logan_2d(500);
+    let arr = shepp_logan_2d(500);
+    export_image(String::from("C:\\Users\\batemanc\\Documents\\source\\test.png"), arr);
     println!("Hello, world!");
+}
+
+
+pub fn export_image(path: String, arr: Array2<f64>) {
+
+    // Convert array for saving
+    let (height, width) = arr.dim();
+    let raw_f64 = arr.into_raw_vec();
+    let vmin = raw_f64.iter().min_by(|a, b| a.partial_cmp(b).unwrap()).expect("Invalid Data in Array."); 
+    let vmax = raw_f64.iter().max_by(|a, b| a.partial_cmp(b).unwrap()).expect("Invalid Data in Array."); 
+    print!("min {}, max {}\n",vmin, vmax);
+    let mut raw: Vec<u8> = Vec::with_capacity(raw_f64.len());
+    for i in 0..raw_f64.len(){
+        raw.push( ((raw_f64[i] - vmin) / (vmax - vmin) * 255.0) as u8 );  // Maximize dynamic range.
+    }
+    print!("height {}, width {}, len {}\n", height, width, raw.len());
+
+    let img = GrayImage::from_raw(width as u32, height as u32, raw)
+        .expect("container should have the right size for the image dimensions");
+
+    let result = img.save(path);
 }
 
 
@@ -31,8 +56,9 @@ pub fn shepp_logan_2d(size: usize) -> Array2<f64>{
         return val <= 1.0
     }
 
-    let p0: f64 = - (size as f64) / 2.0; 
     let dp: f64 = 2.0 / (size as f64);
+    let p0: f64 = - 1.0 + dp / 2.0; 
+
 
     for h in 0..size {
         for w in 0..size {
@@ -48,11 +74,11 @@ pub fn shepp_logan_2d(size: usize) -> Array2<f64>{
                 frame[(h, w)] += -0.98;
             }
             // Ellipse (c)
-            if inside_ellipse(p, (0.22, 0.0), 0.11, 0.31, -18.0){
+            if inside_ellipse(p, (0.22, 0.0), 0.11, 0.31, -0.3141592653589793){  // -18 deg
                 frame[(h, w)] += -0.02;
             }
             // Ellipse (d)
-            if inside_ellipse(p, (-0.22, 0.0), 0.16, 0.41, 18.0){
+            if inside_ellipse(p, (-0.22, 0.0), 0.16, 0.41, 0.3141592653589793){  // +18deg
                 frame[(h, w)] += -0.02;
             }
             // Ellipse (e)
@@ -81,6 +107,7 @@ pub fn shepp_logan_2d(size: usize) -> Array2<f64>{
             }
         }
     }
+    print!("point {}\n", frame[(200, 200)]);
 
     return frame
 }
